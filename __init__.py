f0b04990d6abe5a9796ca913a4fd485d63429027
Timmy Ryan <97642935+Timmy0x@users.noreply.github.com>
2024-03-20 15:07:20 -0600

    fixes and improvements

--- __init__.py
+++ __init__.py
@@ -18,46 +18,44 @@ class Model:
         self.checkfreq = checkfreq
         self.errorcallback = errorcallback
         
-        if list(str(os.getenv("PROVIDER")))[0].lower() == "h":
-            self.provider = Web3(Web3.HTTPProvider(str(os.getenv("PROVIDER"))))
-        elif list(str(os.getenv("PROVIDER")))[0].lower() == "w":
-            self.provider = Web3(Web3.WebsocketProvider(str(os.getenv("PROVIDER"))))
+        if list(str(provider))[0].lower() == "h":
+            self.provider = Web3(Web3.HTTPProvider(str(provider)))
+        elif list(str(provider))[0].lower() == "w":
+            self.provider = Web3(Web3.WebsocketProvider(str(provider)))
                 
-        self.contract_abi = json.loads(open("data/contract_abi.json", "r").read())
-        self.contract_address = Web3.to_checksum_address(open("data/contract_address.txt", "r").read())
+        self.contract_abi = json.loads(open("gator/data/contract_abi.json", "r").read())
+        self.contract_address = Web3.to_checksum_address(open("gator/data/contract_address.txt", "r").read())
         
         self.private_key = env["PRIVATE_KEY"]
         self.public_key = Web3.to_checksum_address(env["PUBLIC_KEY"])
 
     def start(self):
-        try:
-            contract = self.provider.eth.contract(address=self.contract_address, abi=self.contract_abi)
-            listener = contract.events.RequestCreated.create_filter(fromBlock='latest')
-            
-            while True:
-                time.sleep(self.checkfreq)
-                for event in listener.get_new_entries():
-                    if int(event["args"]["model"]) == self.id:
-                        print("------------------------------\n")
-                        print("   Request:     " + str(event["args"]["requestId"]))
-                        print("   - Origin:         " + str(event["args"]["origin"]))
-                        print("   - Confirmations:  " + str(event["args"]["confirmations"]))
-                        print("   - Bid:            " + str(event["args"]["bid"]))
-                        print("   - Prompt:         " + str(event["args"]["prompt"]))
-                        print("   - Model:          " + str(event["args"]["model"]))
-                        print("   - Entropy:        " + str(event["args"]["entropy"]))
-                        print("------------------------------\n")
-                        
+        known_ids = []
+
+        contract = self.provider.eth.contract(address=self.contract_address, abi=self.contract_abi)
+        listener = contract.events.RequestCreated.create_filter(fromBlock='latest')
+        
+        while True:
+            time.sleep(self.checkfreq)
+
+            try:
+                for event in listener.get_all_entries():
+                    if int(event["args"]["modelId"]) == self.id and event["args"]["requestId"] not in known_ids:
+                        print("---------------------------------------------------------------------\n")
+                        print("   Request")
+                        print("      Model:    " + str(event["args"]["modelId"]))
+                        print("      Prompt:   " + str(event["args"]["prompt"]))
+                        print("\n---------------------------------------------------------------------\n")
 
                         result = self.callback(self.id, str(event["args"]["prompt"]), int(event["args"]["entropy"]))
 
-                        function = contract.functions.submitResponse(event["args"]["requestId"], event["args"]["model"], str(result))
+                        function = contract.functions.fufillRequest(event["args"]["requestId"], event["args"]["modelId"], str(result))
                         nonce = self.provider.eth.get_transaction_count(self.public_key)
 
                         gasEstimate = function.estimate_gas({'from': self.public_key})
                         gasPrice = self.provider.eth.generate_gas_price()
                         if gasPrice is None:
-                            gasPrice = self.provider.eth.gas_price * 10
+                            gasPrice = self.provider.eth.gas_price * 25
 
                         estimated_gas_cost = gasEstimate * gasPrice
                         balance = self.provider.eth.get_balance(self.public_key)
@@ -65,7 +63,7 @@ class Model:
                             raise Exception(f"ERROR: insufficient funds. Have {balance}, need {estimated_gas_cost}")
                         else:
                             txn_dict = function.build_transaction({
-                                'gas': int(gasEstimate * 5 * self.boost),
+                                'gas': int(gasEstimate * self.boost),
                                 'gasPrice': int(gasPrice * self.boost),
                                 'nonce': nonce,
                                 'from': self.public_key
@@ -74,9 +72,12 @@ class Model:
                             signed_txn = self.provider.eth.account.sign_transaction(txn_dict, private_key=self.private_key)
                             
                             tx = self.provider.eth.send_raw_transaction(signed_txn.rawTransaction)
-                            print("\n    Success")
-                            print("\n     - Request ID  : " + str(event["args"]["requestId"]))
-                            print("\n     - TX          : " + str(tx.hex()) + "\n")
+                            print("---------------------------------------------------------------------\n")
+                            print("    Success")
+                            print("         TX: " + str(tx.hex()) + "\n")
+                            print("---------------------------------------------------------------------\n")
+                        known_ids.append(event["args"]["requestId"])
         
-        except Exception as e:
-            self.errorcallback(e)
+            except Exception as e:
+                print(e)
+                self.start()
